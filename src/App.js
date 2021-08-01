e29e7466387ba0aae0d76217d03dce0e6389afd7
Filopateer Maged <filopateermaghed@gmail.com>
2021-08-01 21:48:57 +0200

    first commit barebones project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Editor from "@monaco-editor/react";
+import { useState } from "react";
 function App() {
+  const [html, setHtml] = useState("")
+  const getHtml = (value) => {
+     setHtml(value);
+     console.log(html);
+  }
+const [css,setCss] = useState("")
+const getCss = (value) => {
+  setCss(value);
+}
+const [js,setJs] = useState("")
+const getJs = (value) => {
+  setJs(value);
+}
+const docu = `<hmtl>${html}</html><style>${css}</style><script>${js}</script>`
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className="container">
+      <div className="HTML">
+        <h1>HTML</h1>
+        <Editor
+          height="30vh"
+          width="500px"
+          defaultLanguage="html"
+          defaultValue="//Write HTML here!"
+          onChange={getHtml}
+        />
+      </div>
+      <div className="CSS">
+        <h1>CSS</h1>
+        <Editor
+          height="30vh"
+          width="500px"
+          defaultLanguage="css"
+          defaultValue="// Write CSS here!"
+          onChange={getCss}
+        />
+      </div>
+      <div className="JavaScript">
+        <h1>javascript</h1>
+        <Editor
+          height="30vh"
+          width="500px"
+          defaultLanguage="javascript"
+          defaultValue="// Write Some JavaScript Here!"
+          onChange={getJs}
+          
+        />
+      </div>
+    </div>
+    <div>
+      <h1>Output</h1>
+    <iframe className="Output" srcDoc={docu} title="hello"/>
+    </div>
     </div>
   );
 }
